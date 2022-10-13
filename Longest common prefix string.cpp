bb2fbdbc5cd3706316afaa34adeadf607e872136
siddharth jain <47204985+siddharth3108@users.noreply.github.com>
2022-10-13 10:31:31 +0530

    Create Longest common prefix string.cpp

---
+++ Longest common prefix string.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+string longestCommonPrefix(string ar[], int n)
+{
+
+	if (n == 0)
+		return "";
+
+	if (n == 1)
+		return ar[0];
+
+	sort(ar, ar + n);
+
+	int en = min(ar[0].size(),
+				ar[n - 1].size());
+
+	string first = ar[0], last = ar[n - 1];
+	int i = 0;
+	while (i < en && first[i] == last[i])
+		i++;
+
+	string pre = first.substr(0, i);
+	return pre;
+}
+
+int main()
+{
+	string ar[] = {"flower","flow","flight"};
+	int n = sizeof(ar) / sizeof(ar[0]);
+	cout << "The longest common prefix is: "
+		<< longestCommonPrefix(ar, n);
+	return 0;
+}
